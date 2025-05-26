f5bb1592c2fa102dd3861922864c417d86045460
KonorX <61805274+KonorX@users.noreply.github.com>
2025-05-26 17:38:26 +0300

    upgraded visuals

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,11 +14,36 @@ namespace RiftAlytics
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    
     public partial class MainWindow : Window
     {
+
+        public string regionSelected {  get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            regionSelected = string.Empty;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(InputText.Text))
+            {
+                Output.Content = InputText.Text + $" {regionSelected}";
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox) sender;
+            if (comboBox.SelectedItem is ComboBoxItem selected && ((ComboBoxItem)comboBox.SelectedItem).Content is not null)
+            {
+
+                regionSelected = selected.Content.ToString();
+            }
+            else { Output.Content = "Region null"; }
         }
     }
 }
